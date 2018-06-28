2d0ea8b6987f282f80b8cd0fd1e44cd785044cfa
Patricia Mayo Tejedor <patricia.mayo@nl.ibm.com>
2018-06-28 12:45:23 +0200

    :heavy_plus_sign: add nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+    // Getter
+    get area() {
+      return this.calcArea();
+    }
+    // Method
+    calcArea() {
+      return this.height * this.width;
+    }
+  }
+
+  const square = new Rectangle(20, 10);
+
+  console.log(square); // 100
+
   res.render('index', { title: 'Express' });
 });
 
