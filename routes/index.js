f3d4a13482a95a52b398bdf294e50314c497869f
Patricia Mayo Tejedor <patricia.mayo@nl.ibm.com>
2018-06-28 12:49:25 +0200

    :construction: Testing import / exports

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Rectangle = require('./../models/rectangle');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  class Rectangle {
-    constructor(height, width) {
-      this.height = height;
-      this.width = width;
-    }
-    // Getter
-    get area() {
-      return this.calcArea();
-    }
-    // Method
-    calcArea() {
-      return this.height * this.width;
-    }
-  }
+
 
   const square = new Rectangle(20, 10);
 
