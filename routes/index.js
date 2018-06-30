050f61ca03c98cc58c7392b9ee8e747abd23bcc8
Patricia Mayo Tejedor <patricia.mayo@nl.ibm.com>
2018-06-30 20:19:02 +0200

    :sparkles: api url with default deep parameter

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const d3GraphsController = require('./d3GraphsController.js');
 const graphNode = require('./../models/graphNode')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/wikiquery', function(req, res, next) {
   
-
-  const deep = 2  
-  const genesisNode = new graphNode("http://www.wikidata.org/entity/Q395","Mathematics", 0)
+  const deep = req.query.deep || 2
+  var id = req.query.id
+  
+  const genesisNode = new graphNode("http://www.wikidata.org/entity/" + id,"Mathematics", 0)
   var graph = {
     graphNodes: [genesisNode],
     graphLinks: []
@@ -16,7 +17,6 @@ router.get('/', function(req, res, next) {
 
   d3GraphsController.generateD3GraphRecursively(deep, 0, graph, genesisNode, "parent").then(graph => {
     d3GraphsController.generateD3GraphRecursively(deep, 0, graph, genesisNode, "children").then(graph => {
-      console.log(graph)
       res.set('Content-Type', 'application/json');
       res.send(graph)
     })
